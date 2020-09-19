5d700130a6e16a822027a4a0365bd360e082b071
Bmongo <1197457466@qq.com>
2020-09-19 21:19:07 +0800

    redux init finished

--- src/common/server-config.js
+++ src/common/server-config.js
@@ -1,4 +1,4 @@
-const devBaseURL = "http://127.0.0.1:1133"
+const devBaseURL = "http://bmongo.top:1133"
 const proBaseURL = "http://bmongo.top:1133"
 
 export const baseURL = process.env.NODE_ENV === "development" ? devBaseURL : proBaseURL;

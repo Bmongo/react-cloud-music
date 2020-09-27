9e06bda324ac058b65d9a513a56fcb9764c4f8c6
Bmongo <1197457466@qq.com>
2020-09-27 19:54:42 +0800

    player start

--- src/components/player/index.js
+++ src/components/player/index.js
@@ -1,10 +1,29 @@
-import React, { memo } from 'react';
+import React, { memo, useState } from 'react';
+
+import { PlayWrapper, Content } from "./style"
 
 const Player = memo(() => {
+	let [lock, setLock] = useState(false)
 	return (
-		<div>
-			Play
-		</div>
+		<PlayWrapper>
+			<div className={"container " + (lock? "lock" : '')}>
+				<div className="bg playbar-img" />
+				<div className="r-left playbar-img">
+					<div className="btn playbar-img" onClick={e => setLock(!lock)} />
+				</div>
+				<div className="r-right playbar-img" />
+				<Content>
+					<div className="btns">
+						<button className="btn prev playbar-img"></button>
+						<button className="btn stop playbar-img"></button>
+						<button className="btn next playbar-img"></button>
+					</div>
+					<div className="head">
+						111
+					</div>
+				</Content>
+			</div>
+		</PlayWrapper>
 	);
 });
 
